0117994db49be0fe0c46f6ca87cfcfcf06f6ac3e
Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>
2023-09-30 21:14:18 +0100

    dev

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,35 @@
+import { deletePost, getById, updatePost } from "@/app/lib/data";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  try {
+    const id = req.url.split("blogs/")[1];
+    const post = getById(id);
+    if (!post) {
+      return NextResponse.json({ message: "ERROR" }, { status: 404 });
+    }
+    return NextResponse.json({ message: "OK", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "ERROR" }, { status: 500 });
+  }
+};
+
+export const PUT = async (req: Request) => {
+  try {
+    const { title, desc } = await req.json();
+    const id = req.url.split("blogs/")[1];
+    updatePost(id, title, desc);
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "ERROR" }, { status: 500 });
+  }
+};
+export const DELETE = async (req: Request) => {
+  try {
+    const id = req.url.split("blogs/")[1];
+    deletePost(id);
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "ERROR" }, { status: 500 });
+  }
+};
